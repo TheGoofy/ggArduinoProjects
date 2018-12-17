8f017f58ea9f02b96406c7db12f6e5143a96859e
Christoph Laimer <cla@gmx.ch>
2018-12-17 22:58:13 +0100

    - control 4 panels, each panel with 8x8 RGB-LEDs (total 256 LEDs)

--- ggDisplayWS2811/ggDisplayWS2811.ino
+++ ggDisplayWS2811/ggDisplayWS2811.ino
@@ -1,44 +1,84 @@
 #include <FastLED.h>
 
-#define LED_PIN_1 12
-#define LED_PIN_2 13
+#define LED_PIN_0 10
+#define LED_PIN_1 11
+#define LED_PIN_2 12
+#define LED_PIN_3 13
 
 #define M_NUM_LEDS 64
 
+CRGB mLEDs0[M_NUM_LEDS];
 CRGB mLEDs1[M_NUM_LEDS];
 CRGB mLEDs2[M_NUM_LEDS];
+CRGB mLEDs3[M_NUM_LEDS];
 
-void ShowColors(const CRGB& aRGB1,
-                const CRGB& aRGB2)
+void ShowColors(const CRGB& aRGB0,
+                const CRGB& aRGB1,
+                const CRGB& aRGB2,
+                const CRGB& aRGB3)
 {
   for (int vIndex = 0; vIndex < M_NUM_LEDS; vIndex++) {
+    mLEDs0[vIndex] = aRGB0;
     mLEDs1[vIndex] = aRGB1;
     mLEDs2[vIndex] = aRGB2;
+    mLEDs3[vIndex] = aRGB3;
   }
   FastLED.show();
 }
 
-void AnimateRed()
+void AnimateRed(const int aValueMax)
 {
-  static int vValueMax = 50;
   static int vValue = 0;
   static int vDelta = 1;
   if (vValue <= 0) vDelta = 1;
-  if (vValue >= vValueMax) vDelta = -1;
+  if (vValue >= aValueMax) vDelta = -1;
   vValue += vDelta;
-  ShowColors(CRGB(2*vValueMax, vValueMax-vValue, vValue),
-             CRGB(2*vValueMax, vValue, vValueMax-vValue));
+  CRGB vColorA(2*aValueMax, aValueMax-vValue, vValue);
+  CRGB vColorB(2*aValueMax, vValue, aValueMax-vValue);
+  ShowColors(vColorA, vColorB, vColorB, vColorA);
+}
+
+void AnimateGreen(const int aValueMax)
+{
+  static int vValue = 0;
+  static int vDelta = 1;
+  if (vValue <= 0) vDelta = 1;
+  if (vValue >= aValueMax) vDelta = -1;
+  vValue += vDelta;
+  CRGB vColorA(vValue, 2*aValueMax, aValueMax-vValue);
+  CRGB vColorB(aValueMax-vValue, 2*aValueMax, vValue);
+  ShowColors(vColorA, vColorB, vColorB, vColorA);
+}
+
+void AnimateBlue(const int aValueMax)
+{
+  static int vValue = 0;
+  static int vDelta = 1;
+  if (vValue <= 0) vDelta = 1;
+  if (vValue >= aValueMax) vDelta = -1;
+  vValue += vDelta;
+  CRGB vColorA(vValue, aValueMax-vValue, 2*aValueMax);
+  CRGB vColorB(aValueMax-vValue, vValue, 2*aValueMax);
+  ShowColors(vColorA, vColorB, vColorB, vColorA);
 }
 
 void setup()
 { 
+  FastLED.addLeds<WS2811, LED_PIN_0, RGB>(mLEDs0, M_NUM_LEDS);
   FastLED.addLeds<WS2811, LED_PIN_1, RGB>(mLEDs1, M_NUM_LEDS);
   FastLED.addLeds<WS2811, LED_PIN_2, RGB>(mLEDs2, M_NUM_LEDS);
-  //ShowColors(CRGB(0,100,20),CRGB(0,0,100)); // warm orange
+  FastLED.addLeds<WS2811, LED_PIN_3, RGB>(mLEDs3, M_NUM_LEDS);
+  ShowColors(CRGB::Black, CRGB::Black, CRGB::Black, CRGB::Black);
 }
 
 void loop()
 {
-  AnimateRed();
-  //delay(1000);
+  const int vValueMax = 50;
+  int vMode = (millis() / 10000) % 3;
+  switch (vMode) {
+    case 0: AnimateRed(vValueMax); break;
+    case 1: AnimateGreen(vValueMax); break;
+    case 2: AnimateBlue(vValueMax); break;
+  }
+  delay(50);
 }
