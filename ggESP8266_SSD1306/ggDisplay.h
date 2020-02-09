276def1a0ccd5bef344097aaf9308a0e582d7c44
Christoph Laimer <cla@gmx.ch>
2020-02-09 17:46:48 +0100

    - esp8266 ssd1306 display: hot plug connection

--- ggESP8266_SSD1306/ggDisplay.h
+++ ggESP8266_SSD1306/ggDisplay.h
@@ -1,19 +1,32 @@
 #pragma once
 
 #include <Arduino.h>
+#include <Wire.h>
 #include <U8g2lib.h>
 #include <vector>
+#include <functional>
 
 class ggDisplay {
 
 public:
 
   ggDisplay()
-  : mU8G2(U8G2_R0, U8X8_PIN_NONE){
+  : mU8G2(U8G2_R0, U8X8_PIN_NONE),
+    mConnectFunc(nullptr),
+    mConnected(false),
+    mUpdate(false),
+    mCursorY(0) {
   }
 
   void Begin() {
+    Wire.begin();
     mU8G2.begin();
+    Connect();
+  }
+
+  void Run() {
+    Connect();
+    Update();
   }
 
   enum tAlign {
@@ -25,26 +38,29 @@ public:
   void SetTitle(const String& aTitle, tAlign aAlign = eAlignLeft) {
     mTitle.mString = aTitle;
     mTitle.mAlign = aAlign;
+    mUpdate = true;
   }
 
   void SetText(int aLine, const String& aText, tAlign aAlign = eAlignLeft) {
     mText.resize(std::max<size_t>(aLine + 1, mText.size()));
     mText[aLine].mString = aText;
     mText[aLine].mAlign = aAlign;
+    mUpdate = true;
   }
 
   void Clear() {
     mTitle.mString = "";
     mText.clear();
+    mUpdate = true;
   }
 
-  void Update() {
-    mCursorY = 0;
-    ClearBuffer();
-    PrintTitle();
-    PrintLine();
-    PrintText();
-    ShowBuffer();
+  bool IsConnected() const {
+    return mConnected;
+  }
+
+  typedef std::function<void (bool aConnected)> tConnectFunc;
+  void OnConnection(tConnectFunc aConnectFunc) {
+    mConnectFunc = aConnectFunc;
   }
 
 private:
@@ -119,10 +135,47 @@ private:
     mU8G2.sendBuffer();
   }
 
+  void Connect() {
+    const uint8_t vAddressI2C = mU8G2.getU8x8()->i2c_address >> 1; // expecting "0x3C" ...
+    Wire.beginTransmission(vAddressI2C);
+    bool vConnected = Wire.endTransmission() == 0;
+    if (mConnected) {
+      mConnected = vConnected;
+      if (!mConnected) {
+        if (mConnectFunc != nullptr) {
+          mConnectFunc(mConnected);
+        }
+      }
+    }
+    else {
+      mConnected = vConnected;
+      if (mConnected) {
+        mU8G2.begin();
+        if (mConnectFunc != nullptr) {
+          mConnectFunc(mConnected);
+        }
+      }
+    }
+  }
+
+  void Update() {
+    if (mUpdate) {
+      mCursorY = 0;
+      ClearBuffer();
+      PrintTitle();
+      PrintLine();
+      PrintText();
+      ShowBuffer();
+      mUpdate = false;
+    }
+  }
+
   mutable U8G2_SSD1306_128X64_NONAME_F_HW_I2C mU8G2;
+  mutable bool mConnected;
+  tConnectFunc mConnectFunc;
 
-  int mCursorY;
-
+  mutable int mCursorY;
+  mutable bool mUpdate;
   cText mTitle;
   std::vector<cText> mText;
   
--- ggESP8266_SSD1306/ggESP8266_SSD1306.ino
+++ ggESP8266_SSD1306/ggESP8266_SSD1306.ino
@@ -6,26 +6,37 @@ ggDisplay mDisplay;
 
 void setup()
 {
+  Serial.begin(115200);
+  Serial.println();
   mDisplay.Begin();
+  mDisplay.OnConnection([] (bool aConnected) {
+    Serial.println(aConnected ? "connected" : "disconnected");
+  });
 }
 
 
 void loop()
 {
-  static ggDisplay::tAlign vAlign = ggDisplay::eAlignLeft;
-  switch (vAlign) {
-    case ggDisplay::eAlignLeft: vAlign = ggDisplay::eAlignCenter; break;
-    case ggDisplay::eAlignCenter: vAlign = ggDisplay::eAlignRight; break;
-    case ggDisplay::eAlignRight: vAlign = ggDisplay::eAlignLeft; break;
+  static unsigned int vMillisLast = 0;
+  unsigned int vMillis = millis();
+
+  if (vMillis - vMillisLast > 2000) {
+
+    static ggDisplay::tAlign vAlign = ggDisplay::eAlignLeft;
+    switch (vAlign) {
+      case ggDisplay::eAlignLeft: vAlign = ggDisplay::eAlignCenter; break;
+      case ggDisplay::eAlignCenter: vAlign = ggDisplay::eAlignRight; break;
+      case ggDisplay::eAlignRight: vAlign = ggDisplay::eAlignLeft; break;
+    }
+
+    mDisplay.SetTitle("192.168.0.255", vAlign);
+    mDisplay.SetText(0,"Temperature: 21.3""\xb0""C", vAlign);
+    mDisplay.SetText(1,"Humidity: 41.2%", vAlign);
+    mDisplay.SetText(2,"Pressure: 870hPa", vAlign);
+    mDisplay.SetText(3,"goofy was here", vAlign);
+
+    vMillisLast = vMillis;
   }
 
-  mDisplay.SetTitle("192.168.0.255", vAlign);
-  mDisplay.SetText(0,"Temperature: 21.3""\xb0""C", vAlign);
-  mDisplay.SetText(1,"Humidity: 41.2%", vAlign);
-  mDisplay.SetText(2,"Pressure: 870hPa", vAlign);
-  mDisplay.SetText(3,"goofy was here", vAlign);
-
-  mDisplay.Update();
-  
-  delay(2000);
+  mDisplay.Run();
 }
