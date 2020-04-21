5139f7c92e759547d2365aa3012da355ee9a1055
Christoph Laimer <cla@gmx.ch>
2020-04-21 23:28:22 +0200

    - testing the PCA9685 PWM module (tune values)

--- ggTest_PWM_Module/ggTest_PWM_Module.ino
+++ ggTest_PWM_Module/ggTest_PWM_Module.ino
@@ -12,8 +12,8 @@ void AnalogWrite(int aChannel, int aValue)
 void setup()
 {
   pwm.begin();
-  pwm.setOscillatorFrequency(28000000);  // The int.osc. is closer to 27MHz
-  pwm.setPWMFreq(1000);  // 1600 is the maximum PWM frequency
+  pwm.setOscillatorFrequency(27900000);  // The int.osc. is closer to 27MHz
+  pwm.setPWMFreq(1600);  // 1600 is the maximum PWM frequency
   for (int vChannel = 0; vChannel < 16; vChannel++) {
     AnalogWrite(vChannel, vChannel * 273);
   }
@@ -21,7 +21,7 @@ void setup()
 
 void loop()
 {
-  static const int vDelay = 500;
+  static const int vDelay = 1000;
   for (int vOn = 1; vOn < 4096; vOn *= 2) {
     AnalogWrite(0, vOn);
     delay(vDelay);
