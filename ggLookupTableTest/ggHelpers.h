d5521fecf1e168eab5112a63030eb7ff3112b4bf
Christoph Laimer <cla@gmx.ch>
2020-07-03 16:24:14 +0200

    implementing and testing new class "ggLookupTableT"

---
+++ ggLookupTableTest/ggHelpers.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include <limits>
+
+template <typename T1, typename T2>
+inline bool ggIsLess(const T1& aValue1, const T2& aValue2) {
+  typedef typename std::common_type<T1,T2>::type tCommonType;
+  return static_cast<tCommonType>(aValue1) < static_cast<tCommonType>(aValue2);
+}
+
+template <typename T1, typename T2>
+inline T1 ggClamp(const T2& aValue, const T1& aValueMin, const T1& aValueMax) {
+  if (ggIsLess(aValue, aValueMin)) return aValueMin;
+  if (ggIsLess(aValueMax, aValue)) return aValueMax;
+  return static_cast<T1>(aValue);
+}
+
+template <typename T1, typename T2>
+inline T1 ggClampNumericLimits(const T2& aValue) {
+  return ggClamp(aValue, std::numeric_limits<T1>::lowest(), std::numeric_limits<T1>::max());
+}
+
+template <typename TOut, typename TIn>
+TOut ggRound(const TIn aValue) {
+  return ggClampNumericLimits<TOut>(aValue);
+}
+
+template <typename TOut>
+TOut ggRound(const float aValue) {
+  if (std::is_floating_point<TOut>()) return static_cast<TOut>(aValue);
+  else return ggClampNumericLimits<TOut>(static_cast<int64_t>(aValue >= 0 ? aValue + 0.5f : aValue - 0.5f));
+}
+
+template <typename TOut>
+TOut ggRound(const double aValue) {
+  if (std::is_floating_point<TOut>()) return static_cast<TOut>(aValue);
+  else return ggClampNumericLimits<TOut>(static_cast<int64_t>(aValue >= 0 ? aValue + 0.5 : aValue - 0.5));
+}
---
+++ ggLookupTableTest/ggLookupTableTest.ino
@@ -0,0 +1,136 @@
+#include "ggHelpers.h"
+#include "ggLookupTableT.h"
+
+
+template <typename TFunc>
+unsigned long ExecutionDuration(TFunc aFunc)
+{
+  unsigned long vMicrosStart = micros();
+  aFunc();
+  unsigned long vMicrosEnd = micros();
+  return vMicrosEnd - vMicrosStart;
+}
+
+
+template <typename TFunc>
+unsigned long ExecutionDuration(uint32_t aLoops, TFunc aFunc)
+{
+  return ExecutionDuration([&] () {
+    for (uint32_t vLoop = 0; vLoop < aLoops; vLoop++) {
+      aFunc();
+    }
+  });
+}
+
+
+template <typename TValue, typename TFunc>
+void Sweep(const TValue& aMin,
+           const TValue& aMax,
+           uint32_t aSteps,
+           TFunc aFunc)
+{
+  TValue vDelta = (aMax - aMin) / (aSteps - 1);
+  for (uint32_t vStep = 0; vStep < aSteps; vStep++) {
+    aFunc(aMin + vStep * vDelta);
+  }
+}
+
+
+template <typename TLookupTable>
+void TestLookupTable(const TLookupTable& aLookupTable,
+                     uint32_t aSweepSteps = 101,
+                     uint32_t aLoops = 50)
+{
+  Serial.printf("Table size = %d (%d bytes)\n", aLookupTable.GetSize(), sizeof(TLookupTable));
+  Serial.printf("%d loops with %d steps from %f to %f\n",
+                aLoops, aSweepSteps, aLookupTable.GetInputMin(), aLookupTable.GetInputMax());
+  {
+    float vOutputSum = 0.0f;
+    unsigned long vDuration = ExecutionDuration(aLoops, [&] () {
+      Sweep(aLookupTable.GetInputMin(), aLookupTable.GetInputMax(), aSweepSteps, [&] (float aInput) {
+        vOutputSum += aLookupTable.GetExact(aInput);
+      });
+    });
+    Serial.printf("\"GetExact\" Duraton = %d us, vOutputSum = %f\n", vDuration, vOutputSum);
+    yield();
+  }
+  {
+    float vOutputSum = 0.0f;
+    unsigned long vDuration = ExecutionDuration(aLoops, [&] () {
+      Sweep(aLookupTable.GetInputMin(), aLookupTable.GetInputMax(), aSweepSteps, [&] (float aInput) {
+        vOutputSum += aLookupTable(aInput);
+      });
+    });
+    Serial.printf("\"Get\" Duraton = %d us, vOutputSum = %f\n", vDuration, vOutputSum);
+    yield();
+  }
+  {
+    float vOutputSum = 0.0f;
+    unsigned long vDuration = ExecutionDuration(aLoops, [&] () {
+      Sweep(aLookupTable.GetInputMin(), aLookupTable.GetInputMax(), aSweepSteps, [&] (float aInput) {
+        vOutputSum += aLookupTable.GetNN(aInput);
+      });
+    });
+    Serial.printf("\"GetNN\" Duraton = %d us, vOutputSum = %f\n", vDuration, vOutputSum);
+    yield();
+  }
+}
+
+
+void setup()
+{
+  Serial.begin(115200);
+  Serial.println();
+
+  auto vLinearFunc = [] (const float& aInput) {
+    return aInput;
+  };
+
+  auto vSinFunc = [] (const float& aInput) {
+    return sin(aInput);
+  };
+
+  auto vPowFunc = [] (const float& aInput) {
+    return (pow(150.0f, aInput) - 1.0f) / (150.0f - 1.0f);
+  };
+
+  auto vLogFunc = [] (const float& aInput) {
+    return (log((150.0f - 1.0f) * aInput + 1.0f) / log(150.0f));
+  };
+
+  {
+    Serial.printf("\nTesting 1:1 Transfer Function\n");
+    ggLookupTableT<float, 6> vLookupTable(0.0f, 1.0f, vLinearFunc);
+    Sweep(-0.25f, 1.25f, 31, [&] (float aInput) {
+      float vOutputA = vLookupTable.GetExact(aInput);
+      float vOutputB = vLookupTable.Get(aInput);
+      float vOutputC = vLookupTable.GetNN(aInput);
+      Serial.printf("%f\t%f\t%f\t%f\n", aInput, vOutputA, vOutputB, vOutputC);
+    });
+  }
+  {
+    Serial.printf("\nTesting Performance \"linear\":\n");
+    ggLookupTableT<float, 101> vLookupTable(0.0f, 1.0f, vLinearFunc);
+    TestLookupTable(vLookupTable);
+  }
+  {
+    Serial.printf("\nTesting Performance \"sin\":\n");
+    ggLookupTableT<float, 501> vLookupTable(0.0f, 2.0f*M_PI, vSinFunc);
+    TestLookupTable(vLookupTable);
+  }
+  {
+    Serial.printf("\nTesting Performance \"pow\":\n");
+    ggLookupTableT<float, 101> vLookupTable(0.0f, 1.0f, vPowFunc);
+    TestLookupTable(vLookupTable);
+  }
+  {
+    Serial.printf("\nTesting Performance \"log\":\n");
+    ggLookupTableT<float, 101> vLookupTable(0.0f, 1.0f, vLogFunc);
+    TestLookupTable(vLookupTable);
+  }
+}
+
+
+void loop()
+{
+}
