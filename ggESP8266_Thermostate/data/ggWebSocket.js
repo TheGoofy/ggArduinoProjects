0d57fcc2d02c21b0a1c42435ebab11d4844fc35f
Christoph Laimer <cla@gmx.ch>
2020-07-31 16:33:57 +0200

    - esp8266 thermostate: own js-file for websocket-connection

---
+++ ggESP8266_Thermostate/data/ggWebSocket.js
@@ -0,0 +1,56 @@
+
+function CreateWebSocket(
+  aParent,
+  aWebSocketObjectName,
+  aHostAddress,
+  aMessageFunc,
+  aReconnectTimeMS,
+  aStatusDiv,
+  aDefaultAddress)
+{
+  if (aHostAddress == '') {
+    if (!aDefaultAddress) aDefaultAddress = "192.168.0.150";
+    aHostAddress = window.prompt('Please enter address of WebSocket server', aDefaultAddress);
+  }
+
+  let vWebSocket = aParent[aWebSocketObjectName];
+  if (!vWebSocket || vWebSocket.readyState == WebSocket.CLOSED) {
+
+    aParent[aWebSocketObjectName] = new WebSocket('ws://' + aHostAddress +':81/', ['arduino']);
+    let vWebSocket = aParent[aWebSocketObjectName];
+    
+    vWebSocket.onopen = function (aEvent) {
+      console.info('OnWebSocketOpen()');
+      if (aStatusDiv) aStatusDiv.innerHTML = 'connected';
+      if (aParent.mWebSocketReConnectTimerID) {
+        window.clearInterval(aParent.mWebSocketReConnectTimerID);
+        aParent.mWebSocketReConnectTimerID = null;
+      }
+    };
+    
+    vWebSocket.onclose = function (aEvent) {
+      console.warn('OnWebSocketClose()');
+      if (aStatusDiv) aStatusDiv.innerHTML = 'disconnected';
+      if (!aParent.mWebSocketReConnectTimerID) {
+        aParent.mWebSocketReConnectTimerID = window.setInterval(function () {
+          CreateWebSocket(aParent, aWebSocketObjectName, aHostAddress, aMessageFunc, aReconnectTimeMS, aStatusDiv);
+        }, aReconnectTimeMS);
+      }
+    };
+    
+    vWebSocket.onerror = function (aEvent) {
+      console.error('OnWebSocketError()');
+      if (aStatusDiv) aStatusDiv.innerHTML = 'error';
+    };
+    
+    vWebSocket.onmessage = function (aEvent) {
+      try {
+        console.log('OnWebSocketMessage() - evaluating: ' + aEvent.data);
+        aMessageFunc(aEvent.data);
+      }
+      catch (vError) {
+        console.error('OnWebSocketMessage() - unable to evaluate: ' + aEvent.data);
+      }
+    };
+  }
+}
