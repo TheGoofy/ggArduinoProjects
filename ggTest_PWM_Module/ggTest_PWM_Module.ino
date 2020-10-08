8926ba5e46c1dc46db6cb8acdbab307fb52601e3
Christoph Laimer <cla@gmx.ch>
2020-10-08 13:22:21 +0200

    - esp8266 thermostate: alert when loading data failed

--- ggTest_PWM_Module/ggTest_PWM_Module.ino
+++ ggTest_PWM_Module/ggTest_PWM_Module.ino
@@ -9,21 +9,68 @@ void AnalogWrite(int aChannel, int aValue)
   pwm.setPin(aChannel, aValue);
 }
 
+const int mButtonPin = A0;
+
 void setup()
 {
+  Serial.begin(115200);
+  Serial.println("ggTestPWM");
   pwm.begin();
   pwm.setOscillatorFrequency(27900000);  // The int.osc. is closer to 27MHz
   pwm.setPWMFreq(1600);  // 1600 is the maximum PWM frequency
   for (int vChannel = 0; vChannel < 16; vChannel++) {
     AnalogWrite(vChannel, vChannel * 273);
   }
+
+  pinMode(3, FUNCTION_3);
+  pinMode(3, INPUT_PULLUP);
+  pinMode(mButtonPin, INPUT_PULLUP);
+  pinMode(12, INPUT_PULLUP);
 }
 
 void loop()
 {
-  static const int vDelay = 1000;
+  /*
+  const int vCount = 10000;
+
+  int vDummy = 0;
+
+  unsigned long vStart = 0;
+  unsigned long vStop = 0;
+
+  vStart = micros();
+  for (int i = 0; i < vCount; i++) {
+    yield();
+  }
+  vStop = micros();
+  unsigned long vYield = vStop - vStart;
+  
+  vStart = micros();
+  for (int i = 0; i < vCount; i++) {
+    vDummy = analogRead(A0);
+    yield();
+  }
+  vStop = micros();
+  unsigned long vAnalog = vStop - vStart;
+
+  vStart = micros();
+  for (int i = 0; i < vCount; i++) {
+    vDummy = digitalRead(12);
+    yield();
+  }
+  vStop = micros();
+  unsigned long vDigital = vStop - vStart;
+
+  Serial.printf("%d\t%d\t%d\t\n", vYield, vDigital, vAnalog);
+  */
+  Serial.printf("digitalread = %d\n", digitalRead(3));
+  // Serial.printf("%d\n", analogRead(mButtonPin));
+  delay(500);
+  /*
+  static const int vDelay = 500;
   for (int vOn = 1; vOn < 4096; vOn *= 2) {
     AnalogWrite(0, vOn);
     delay(vDelay);
   }
+  */
 }
