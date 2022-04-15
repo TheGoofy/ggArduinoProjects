d6e7d4ed5635d231c8a37eb36ec652d898773a1b
Christoph Laimer <cla@gmx.ch>
2022-04-15 11:46:40 +0200

    - I2C scanner for arduino/esp/teensey

--- ggScanI2C/ggScanI2C.ino
+++ ggScanI2C/ggScanI2C.ino
@@ -6,10 +6,14 @@
 
 void setup()
 {
-  Wire.begin(); // A4: SDA, A5: SCL // arduino
-  // Wire.begin(SDA_PIN, SCL_PIN); // esp8266
+  // initialize I2C interface
+  // Wire.begin(); // A4: SDA, A5: SCL // arduino
+  Wire.begin(SDA_PIN, SCL_PIN); // esp8266
   // Wire.begin(I2C_MASTER, 0x00, I2C_PINS_18_19, I2C_PULLUP_INT, 800000); // teensy3
+
+  // initialize serial communication (for debugging output only)
   Serial.begin(115200);
+  while (!Serial) {}
   Serial.println("\nI2C Scanner");
 }
 
@@ -31,7 +35,7 @@ void loop()
       Serial.print("I2C device found at address 0x");
       if (address<16) Serial.print("0");
       Serial.print(address,HEX);
-      Serial.println("  !");
+      Serial.println();
       nDevices++;
     }
     else if (error == 4) {
@@ -41,10 +45,9 @@ void loop()
     }    
   }
   
-  if (nDevices == 0) Serial.println("No I2C devices found\n");
-  else Serial.println("done\n");
+  if (nDevices == 0) Serial.print("No I2C devices found\n\n");
+  else Serial.printf("Found %d I2C devices\n\n", nDevices);
 
   // wait 5 seconds for next scan
   delay(5000);
 }
-
