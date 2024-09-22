fefd331b99a8ae7f26c15bcf975a5c78aaa72af5
Christoph Laimer <cla@gmx.ch>
2024-09-22 14:25:27 +0200

    attiny PC desk fan: FanLEDs animation #2

--- ggAttiny_Fan/ggFanLEDs.h
+++ ggAttiny_Fan/ggFanLEDs.h
@@ -28,28 +28,60 @@ public:
   }
 
   void Update() {
-    static int8_t vIndexY = 0;
-    static int8_t vIncY = 1;
-    if (vIndexY < 0) {
-      vIndexY = 0;
-      vIncY = 1;
+    static uint8_t vAnimation = 0;
+    if (vAnimation / 5 % 2 == 0) {
+      static int8_t vIndexY = 0;
+      static int8_t vIncY = 1;
+      if (vIndexY < 0) {
+        vIndexY = 0;
+        vIncY = 1;
+        vAnimation++;
+      }
+      else if (vIndexY >= GetSizeY()) {
+        vIndexY = GetSizeY() - 1;
+        vIncY = -1;
+      }
+      if (vIndexY > 0) {
+        SetPixelXY(0, vIndexY - 1, GetColor(50, 0, 4));
+        SetPixelXY(1, vIndexY - 1, GetColor(50, 4, 0));
+      }
+      SetPixelXY(0, vIndexY, GetColor(255, 100, 0));
+      SetPixelXY(1, vIndexY, GetColor(255, 100, 0));
+      if (vIndexY < GetSizeY() - 1) {
+        SetPixelXY(0, vIndexY + 1, GetColor(3, 0, 8));
+        SetPixelXY(1, vIndexY + 1, GetColor(0, 3, 8));
+      }
+      Show();
+      vIndexY = vIndexY + vIncY;
     }
-    else if (vIndexY >= GetSizeY()) {
-      vIndexY = GetSizeY() - 1;
-      vIncY = -1;
+    if (vAnimation / 5 % 2 == 1) {
+      static int8_t vIndexX = 0;
+      static int8_t vIncX = 1;
+      if (vIndexX < 0) {
+        vIndexX = 0;
+        vIncX = 1;
+        vAnimation++;
+      }
+      else if (vIndexX >= GetSizeX_H()) {
+        vIndexX = GetSizeX_H() - 1;
+        vIncX = -1;
+      }
+      if (vIndexX > 0) {
+        for (uint8_t vIndexY = 0; vIndexY < GetSizeY_H(); vIndexY++) {
+          SetPixelXY_H(vIndexX - 1, vIndexY, GetColor(50, 0, 0));
+        }
+      }
+      for (uint8_t vIndexY = 0; vIndexY < GetSizeY_H(); vIndexY++) {
+        SetPixelXY_H(vIndexX, vIndexY, GetColor(255, 100, 0));
+      }
+      if (vIndexX < GetSizeX_H() - 1) {
+        for (uint8_t vIndexY = 0; vIndexY < GetSizeY_H(); vIndexY++) {
+          SetPixelXY_H(vIndexX + 1, vIndexY, GetColor(0, 0, 8));
+        }
+      }
+      Show();
+      vIndexX = vIndexX + vIncX;
     }
-    if (vIndexY > 0) {
-      SetPixelXY(0, vIndexY - 1, GetColor(50, 0, 4));
-      SetPixelXY(1, vIndexY - 1, GetColor(50, 4, 0));
-    }
-    SetPixelXY(0, vIndexY, GetColor(255, 100, 0));
-    SetPixelXY(1, vIndexY, GetColor(255, 100, 0));
-    if (vIndexY < GetSizeY() - 1) {
-      SetPixelXY(0, vIndexY + 1, GetColor(3, 0, 8));
-      SetPixelXY(1, vIndexY + 1, GetColor(0, 3, 8));
-    }
-    Show();
-    vIndexY = vIndexY + vIncY;
   }
 
 private:
@@ -71,10 +103,25 @@ private:
   }
 
   inline uint8_t GetIndexXY(uint8_t aIndexX, uint8_t aIndexY) const {
-    static constexpr uint8_t vBlockSize = TNumLEDs / GetSizeX();
+    static constexpr uint8_t vBlockSize = TNumLEDs / 2;
     const uint8_t vFanIndex = aIndexY / vBlockSize;
     const uint8_t vLedIndex = aIndexY % vBlockSize;
     return GetIndex(vFanIndex, aIndexX == 0 ? vLedIndex : TNumLEDs - vLedIndex - 1);
+  }
+
+  static constexpr uint8_t GetSizeX_H() {
+    return TNumFans * TNumLEDs / GetSizeY_H();
+  }
+
+  static constexpr uint8_t GetSizeY_H() {
+    return 2 * TNumFans;
+  }
+
+  inline uint8_t GetIndexXY_H(uint8_t aIndexX, uint8_t aIndexY) const {
+    static constexpr uint8_t vBlockSize = TNumLEDs / 2;
+    const uint8_t vFanIndex = aIndexY / 2;
+    const uint8_t vLedIndex = (aIndexX + vBlockSize / 2) % TNumLEDs;
+    return GetIndex(vFanIndex, aIndexY % 2 == 0 ? (TNumLEDs + vBlockSize - vLedIndex - 1) % TNumLEDs : vLedIndex);
   }
 
   static constexpr uint32_t GetColor(uint8_t aR, uint8_t aG, uint8_t aB) {
@@ -87,6 +134,10 @@ private:
 
   inline void SetPixelXY(uint8_t aIndexX, uint8_t aIndexY, uint32_t aColor) {
     mLEDs.setPixelColor(GetIndexXY(aIndexX, aIndexY), aColor);
+  }
+
+  inline void SetPixelXY_H(uint8_t aIndexX, uint8_t aIndexY, uint32_t aColor) {
+    mLEDs.setPixelColor(GetIndexXY_H(aIndexX, aIndexY), aColor);
   }
 
   inline void Show() {
